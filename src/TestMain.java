639ba891ccefd9707ee1be437187fdbac760a786
Karan <karanpatel4567@gmail.com>
2017-10-28 09:57:31 -0700

    Removed TestMain.java from Git

--- src/TestMain.java
+++ src/TestMain.java
@@ -1,14 +1,9 @@
+import LanguageCore.Enumerations;
+
 public class TestMain {
 
     public static void main(String[] args){
 
-        try{
-            throw new ArithmeticException("Math error you dumb ass.");
-        }catch(Exception e){
-            e.printStackTrace();
-        }
-        finally{
-            System.out.println("finally gets executed before catch statement.");
-        }
+        Enumerations.enumDemo();
     }
 }
