f941b860ce1a3402e73b5eab43889557320d8831
Karan Patel <karanpatel4567@gmail.com>
2018-07-29 10:44:21 -0700

    Adding CommandLineRunner

--- spring-boot/src/main/java/com/ker/springboot/Application.java
+++ spring-boot/src/main/java/com/ker/springboot/Application.java
@@ -1,5 +1,7 @@
 package com.ker.springboot;
 
+import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -13,13 +15,21 @@ Classes market with @Component
 What are the dependencies for a bean?
 fields marked with @Autowired annotation
  */
+@Log4j2
 @SpringBootApplication // initializes application context, enables auto-configuration, scans the package and sub-packages that it is defined in
 // Where does Spring search for Beans? -? Spring automatically checks the package (i.e. com.ker.springboot) and the sub-packages for components (i.e. component scan)
 @ComponentScan(value = {"com.ker.springboot"}) // explicitly specifying packages to search for components
 @PropertySource("classpath:app.properties")     // application.properties is automatically recognised by Spring Boot
-public class Application {
+public class Application implements CommandLineRunner {
 
   public static void main(String[] args) {
+    System.out.println("Before app start...");
     ApplicationContext applicationContext = SpringApplication.run(Application.class, args);
+    log.info("App context loaded!");
+  }
+
+  @Override
+  public void run(String... args) throws Exception {
+    log.info("Will be invoked as soon as the application is launched. Since the method is not static, can autowire beans.");
   }
 }
