e78663ab36adcf6d646399b6cb8f902ee56d7052
Karan Patel <karanpatel4567@gmail.com>
2018-07-28 20:37:04 -0700

    Updating commentgm

--- spring-boot/src/main/java/com/ker/springboot/Application.java
+++ spring-boot/src/main/java/com/ker/springboot/Application.java
@@ -13,9 +13,8 @@ Classes market with @Component
 What are the dependencies for a bean?
 fields marked with @Autowired annotation
  */
-@SpringBootApplication // initializes application context
-// Where does Spring search for Beans?
-// Spring automatically checks the package (i.e. com.ker.springboot) and the sub-packages for components (i.e. component scan)
+@SpringBootApplication // initializes application context, enables auto-configuration, scans the package and sub-packages that it is defined in
+// Where does Spring search for Beans? -? Spring automatically checks the package (i.e. com.ker.springboot) and the sub-packages for components (i.e. component scan)
 @ComponentScan(value = {"com.ker.springboot"}) // explicitly specifying packages to search for components
 @PropertySource("classpath:app.properties")     // application.properties is automatically recognised by Spring Boot
 public class Application {
