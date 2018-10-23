de727221a1b5f94b60528fe0139ade5b16357499
Karan Patel <karanpatel4567@gmail.com>
2018-10-22 21:36:31 -0700

    Comments

--- java-lang/src/main/java/libraries/LibDemo.java
+++ java-lang/src/main/java/libraries/LibDemo.java
@@ -2,12 +2,10 @@ package libraries;
 
 import lombok.extern.log4j.Log4j2;
 
-import java.util.List;
-
 @Log4j2
 public class LibDemo {
     public static void demo(){
-        List<Integer> integerList = List.of(1, 2, 3, 4);
-        log.debug(integerList);
+        //List<Integer> integerList = List.of(1, 2, 3, 4);
+        //log.debug(integerList);
     }
 }
