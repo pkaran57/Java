d541b101f101adc49924e0488d3f8d9d6b635eac
Karan Patel <karanpatel4567@gmail.com>
2019-01-27 20:52:01 -0800

    Adding java-aws module

---
+++ java-aws/src/main/java/com/ker/aws/Main.java
@@ -0,0 +1,35 @@
+package com.ker.aws;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.CreateBucketRequest;
+import software.amazon.awssdk.services.s3.model.CreateBucketResponse;
+import software.amazon.awssdk.services.s3.model.ListBucketsRequest;
+
+@Slf4j
+@SpringBootApplication
+@PropertySource(value = "classpath:application.yml")
+public class Main {
+
+    public static void main(String[] args) {
+        System.setProperty("aws.accessKeyId", "");
+        System.setProperty("aws.secretAccessKey", "");
+
+        try(S3Client s3Client = S3Client.builder()
+                .region(Region.US_WEST_2)
+                .build()) {
+
+            final String BUCKET_NAME = "ker-bucket";
+
+            s3Client.listBuckets(ListBucketsRequest.builder().build());
+
+            CreateBucketRequest createBucketRequest = CreateBucketRequest.builder().bucket(BUCKET_NAME).build();
+            CreateBucketResponse createBucketResponse = s3Client.createBucket(createBucketRequest);     // http://ker-bucket.s3.amazonaws.com/
+
+            log.info(createBucketResponse.toString());
+        }
+    }
+}
