3f84780ea4e9ee23c26afb1c745fe88d64bd0c01
Karan Patel <karanpatel4567@gmail.com>
2022-06-25 17:50:25 -0700

    Updating java-sandbox

--- java-sandbox/src/main/java/com/ker/App.java
+++ java-sandbox/src/main/java/com/ker/App.java
@@ -1,7 +1,11 @@
 package com.ker;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class App {
-    public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
